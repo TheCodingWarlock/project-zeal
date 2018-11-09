10230049d6f4849c7a132029f6008a608666f638
EdwardMuturi <kakaturi300@gmail.com>
2018-11-09 19:04:19 +0300

    Added initial files

---
+++ app/src/androidTest/java/com/deedcorps/edward/project_zeal/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.deedcorps.edward.project_zeal
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.deedcorps.edward.project_zeal", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/deedcorps/edward/project_zeal/FloatingServiceActivity.kt
@@ -0,0 +1,12 @@
+package com.deedcorps.edward.project_zeal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FloatingServiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_floating_service)
+    }
+}
