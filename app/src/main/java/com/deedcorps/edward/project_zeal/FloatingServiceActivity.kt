16408eb89415a1fc7c7bd3182d940405cb40323d
EdwardMuturi <kakaturi300@gmail.com>
2018-11-09 19:40:28 +0300

    Migrating to android X

--- app/src/main/java/com/deedcorps/edward/project_zeal/FloatingServiceActivity.kt
+++ app/src/main/java/com/deedcorps/edward/project_zeal/FloatingServiceActivity.kt
@@ -1,6 +1,6 @@
 package com.deedcorps.edward.project_zeal
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FloatingServiceActivity : AppCompatActivity() {
